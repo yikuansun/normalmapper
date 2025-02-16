62145716074e9d412475be02857ed212387ab734
Yikuan Sun <yikuansun2015@gmail.com>
2025-02-15 19:34:32 -0500

    Cap size at 1024x1024

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from PIL import Image
 from imaginairy_normal_map.model import create_normal_map_pil_img
 
 img = Image.open("oval-office-large.jpg")
+img.thumbnail((1024, 1024), Image.ANTIALIAS)
 normal_img = create_normal_map_pil_img(img)
 normal_img.save("oval-office-large-normal.jpg")
