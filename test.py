e1cc6e130178966c87b746842b3622599a1de754
Yikuan Sun <yikuansun2015@gmail.com>
2025-05-03 12:39:53 -0400

    delete all the code
    :((((

--- test.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from imaginairy_normal_map.model import create_normal_map_pil_img
-
-img = Image.open("oval-office-large.jpg")
-img.thumbnail((1024, 1024), Image.ANTIALIAS)
-normal_img = create_normal_map_pil_img(img)
-normal_img.save("oval-office-large-normal.jpg")
